1543a4878e2e6e35430ade378ac379027de790c2
david <ds@dalsegno-solutions.com>
2023-05-20 19:36:03 +0200

    Example

---
+++ README.md
@@ -0,0 +1,3 @@
+# initial_message
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseMessaging.instance.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+  runApp(const MainApp());
+}
+
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+      const Duration(seconds: 1),
+      () async {
+        await FirebaseMessaging.instance.getInitialMessage();
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(title: Text('X')),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
